bb49c29b5758ac76ad63544de5e537476db304c6
zsy <zsy@qq.com>
2023-06-04 16:51:32 +0800

    third

--- src/main/java/com/zsy/git/GitTset.java
+++ src/main/java/com/zsy/git/GitTset.java
@@ -4,5 +4,6 @@ public class GitTset {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world1");
+        System.out.println("hello world3");
     }
 }
