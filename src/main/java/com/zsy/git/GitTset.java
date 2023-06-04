2a219850ba1d4869676ad3bf223a9188b43eb94c
zsy <zsy@qq.com>
2023-06-04 22:55:11 +0800

    git commit2

--- src/main/java/com/zsy/git/GitTset.java
+++ src/main/java/com/zsy/git/GitTset.java
@@ -3,14 +3,5 @@ package com.zsy.git;
 public class GitTset {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world1");
-        System.out.println("hello world3");
-        System.out.println("hello world4");
-        System.out.println("master test");
-        System.out.println("hot-fix");
-        System.out.println("push-fix");
-        System.out.println("pull-fix");
-        System.out.println("gitee test");
-        System.out.println("gitee test1");
     }
 }
