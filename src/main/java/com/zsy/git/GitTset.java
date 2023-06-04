cb34e0f8aa3b534f13119ffea98dcf4c30ecd282
zsy <zsy@qq.com>
2023-06-04 19:03:54 +0800

    hot-fix commit2

--- src/main/java/com/zsy/git/GitTset.java
+++ src/main/java/com/zsy/git/GitTset.java
@@ -6,5 +6,6 @@ public class GitTset {
         System.out.println("hello world1");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("hot-fix");
     }
 }
